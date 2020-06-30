23f6d281c860f35c10027f8e56a8a1281a519d43
Dan Dumitriu <dandumitriu33@gmail.com>
2020-06-30 12:34:52 +0300

    Add read emails from Gmail.IMAP backend only

--- SaintSender.DesktopUI/Views/MainWindow.xaml.cs
+++ SaintSender.DesktopUI/Views/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SaintSender.Core.Services;
+using ActiveUp.Net.Mail;
 
 namespace SaintSender.DesktopUI
 {
@@ -32,6 +33,42 @@ namespace SaintSender.DesktopUI
             var name = NameTxt.Text;
             var greeting = service.Greet(name);
             ResultTxt.Text = greeting;
+
+            
+        }
+
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var emailList = getEmailsAsync();
+            foreach (Message email in emailList)
+            {
+                Console.WriteLine($"From: {email.From}, Subject: {email.Subject}");
+                //Console.WriteLine("<p>{0}: {1}</p><p>{2}</p>", email.From, email.Subject, email.BodyHtml.Text);
+                //if (email.Attachments.Count > 0)
+                //{
+                //    foreach (MimePart attachment in email.Attachments)
+                //    {
+                //        Console.WriteLine("<p>Attachment: {0} {1}</p>", attachment.ContentName, attachment.ContentType.MimeType);
+
+                //    }
+                //}
+            }
+        }
+
+        public IEnumerable<Message> getEmailsAsync()
+        {
+            var mailRepository = new MailRepository(
+                            "imap.gmail.com",
+                            993,
+                            true,
+                            "ionionescu2020demo@gmail.com",
+                            "demoaccountpassword"
+                        );
+
+            var emailList = mailRepository.GetAllMails("inbox");
+            return emailList;
         }
     }
+
 }
