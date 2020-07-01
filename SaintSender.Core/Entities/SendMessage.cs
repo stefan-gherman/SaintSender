0889aabcfcb9aeddff2b0c0b9b38d838c19cb501
Claudiu <cduneata@gmail.com>
2020-07-01 12:43:08 +0300

    Add send message logic

--- SaintSender.Core/Entities/SendMessage.cs
+++ SaintSender.Core/Entities/SendMessage.cs
@@ -1,17 +1,84 @@
-﻿using System;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using SaintSender.Core.Services;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SaintSender.Core.Entities
 {
-    class SendMessage
+    public class SendMessage
     {
+        private readonly UserData userData;
+        private MimeMessage messageToSend;
+
+        public SendMessage()
+        {
+            if (File.Exists(Environment.CurrentDirectory + "//credentials.json"))
+            {
+                userData = JsonService.DeserializeJsonFile(Environment.CurrentDirectory + "//credentials.json");
+
+            }
+        }
+
         public string To { get; set; }
 
         public string Subject { get; set; }
 
         public string Message { get; set; }
+
+
+        /// <summary>
+        /// Set subject, message and where to send the email
+        /// </summary>
+        private void SetSendmessageData()
+        {
+
+            messageToSend = new MimeMessage
+            {
+                Sender = new MailboxAddress("Ion Ionescu", userData.Email),
+                Subject = Subject
+            };
+
+            messageToSend.Body = new TextPart(MimeKit.Text.TextFormat.Plain)
+            {
+                Text = Message
+            };
+
+            messageToSend.To.Add(address: new MailboxAddress(To));
+
+        }
+
+        /// <summary>
+        /// Connect to gmail smtp and send message
+        /// </summary>
+        private async void ConnectAndSendEmail()
+        {
+            using (var smtp = new SmtpClient())
+            {
+                smtp.MessageSent += (sender, args) =>
+                {
+                    Console.WriteLine("Email was sent " + args.Response) ;
+                };
+
+                await smtp.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync(userData.Email, userData.Password);
+                await smtp.SendAsync(messageToSend);
+                await smtp.DisconnectAsync(true);
+            }
+        }
+
+        /// <summary>
+        /// Call SetMessaeData() and ConnectAndSendEmail()
+        /// </summary>
+        public void SendMessageToEmail()
+        {
+            SetSendmessageData();
+            ConnectAndSendEmail();
+        }
     }
 }
