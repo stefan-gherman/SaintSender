e34f457f87809c6cb503dfffd9cb20b1155e0751
Stefan Gherman <scgheman@gmail.com>
2020-07-01 08:19:40 +0300

    Provide Implementation for NLMAPICheck Method via NETWORKLIST

---
+++ SaintSender.Core/Services/ConnectionService.cs
@@ -0,0 +1,58 @@
+﻿using NETWORKLIST;
+using SaintSender.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender.Core.Services
+{
+    public class ConnectionService : IWebConnectionService
+    {
+        public bool ConnectionCheck(string url)
+        {
+            try
+            {
+                using (WebClient client = new WebClient() { Proxy = null})
+                {
+                    using (client.OpenRead(url))
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool NLMAPICheck()
+        {
+            INetworkListManager networkListManager = new NetworkListManager();
+            return networkListManager.IsConnectedToInternet;
+        }
+
+        public bool PingChek(string url)
+        {
+            try {
+                Ping myPing = new Ping();
+                PingReply reply = myPing.Send(url, 2000);
+                if(reply != null)
+                {
+
+                    return true; 
+                }
+                return false;
+            } 
+            catch(Exception)
+            {
+                return false;
+            }
+            
+        }
+    }
+}
