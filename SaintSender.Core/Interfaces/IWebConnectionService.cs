99bb681754cb115a71c2526287fdfa8c131cb98e
Stefan Gherman <scgheman@gmail.com>
2020-07-01 06:50:50 +0300

    Add interface to check web connection

---
+++ SaintSender.Core/Interfaces/IWebConnectionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender.Core.Interfaces
+{
+    public interface IWebConnectionService
+    {
+        bool ConnectionCheck(string url);
+        bool PingChek(string url);
+    }
+}
