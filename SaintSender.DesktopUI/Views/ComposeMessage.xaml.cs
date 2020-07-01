52a5630021d9c833e3e212130857326b2b9aabfb
Claudiu <cduneata@gmail.com>
2020-07-01 12:43:39 +0300

    Move xaml view to View folder

---
+++ SaintSender.DesktopUI/Views/ComposeMessage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using SaintSender.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SaintSender.DesktopUI.ViewModels
+{
+    /// <summary>
+    /// Interaction logic for ComposeMessage.xaml
+    /// </summary>
+    public partial class ComposeMessage : Window
+    {
+        
+
+        public ComposeMessage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Close Compose Message window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseComposeMesssageWindow_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to discard the message ?", "My App", MessageBoxButton.YesNo);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    Close();
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+        }
+
+        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage message = new SendMessage()
+            {
+                To = SendTo.Text,
+                Subject = SendSubject.Text,
+                Message = SendMessage.Text
+            };
+
+            message.SendMessageToEmail();
+            Close();
+
+            
+        }
+    }
+}
